d1e8fc7e17d0ebecd5000c869b163d143acdce22
cjmess01 <cjmess01@gmail.com>
2026-02-11 10:03:17 -0600

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+This is Caleb Grep
+Crep
+
+I want to start by makign it jsut search through a single file super quick...
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Usage: crep <pattern> <filename> -flags");
+    println!("Flags:");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Query: {input}");
+}
