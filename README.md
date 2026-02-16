dae0a667421808df59191c6659fd3cc1d6e28385
cjmess01 <cjmess01@gmail.com>
2026-02-16 10:00:02 -0600

    another commit

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 This is Caleb Grep
 Crep
 
+2/11/26 2:00pm
 I want to start by making it just search through a single file super quick
 This can be accomplished by looking at the file path
 If the file path /path/to/file.txt
 we can then use fs::metadata to get the information about it.
+
+2/11/26 7:51pm
+Ok I did line major searching, but I want to use the boyer moore algorithm that is actually used in for real Grep
+before I do that though, I want to make the row major not just print matches, but return them as a list of "match" structs.
+So now I'll do this struct business.
+
+2/11/26 8:37
+Ok I though about it and making a struct and saving results will really bloat this thing like crazy
+Large files would get these crazy long vectors and this would not be good....
+Instead I will just make it print and forget using a "print_match" function.
+
+2/11/26 9:00
+OK I removed struct and just have it print. It works but doesn't work for Regex yet. I think making it match regex on line major is next goal
+After that, I will research the boyer moore algorithm and implement that
+
+2/12/26 3:44pm
+I think it would be cool to do a limit on the recursive call for files.
+
+I am going to see about Regex pattern matching on single files now. then...
+I am going to do folder searching and it will just do a single folder.
+Flags will be integrated later.
+I want to do a callback for filesearching within the folder search
+
+2/12/26 4:17
+Ok basic REGEX works, "cargo run "(?i)rust" .\tests\test_input1.txt" worked which was really cool
+It is printing separately in each line, which I would prefer it do each match on one line
+next goal is to do that,
+
+single line, multiple match -> folder searching w/ line major callback -> flags -> boyer moore.
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,86 @@
 use std::env;
 use std::fs;
-use std::process::exit;
+use std::io::BufRead;
+use std::io::BufReader;
 
-fn main() {
+use regex::Regex;
+
+fn main() -> std::process::ExitCode {
     let args: Vec<String> = env::args().collect();
 
-    // Pulls args[1] and args[2] and sets them to pattern and filename
-    // if case of <3 args, exits
     let (pattern, filename) = match args.as_slice() {
         [_, a, b, ..] => (a, b),
         _ => {
-            println!("Usage: crep <pattern> <filename> -flags");
-            exit(100);
+            eprintln!("Usage: crep <pattern> <filename> -flags");
+            return std::process::ExitCode::from(2);
         }
     };
 
     let mtd = match fs::metadata(filename) {
         Ok(mtd) => mtd,
         Err(_) => {
-            println!("{filename} is not a file or directory");
-            exit(100);
+            eprintln!("{filename} is not a file or directory");
+            return std::process::ExitCode::from(2);
         }
     };
 
+    let pattern = match Regex::new(pattern) {
+        Ok(p) => p,
+        Err(_) => {
+            eprintln!("{pattern} is not a valid regex");
+            return std::process::ExitCode::from(2);
+        }
+    };
+
+    println!("Pattern requested: {pattern}");
+
     if mtd.file_type().is_file() {
-        println!("I am a file");
+        let total_matches = crep_file_line_major(filename, &pattern).expect("Error!");
+        println!("Total Matches: {total_matches}");
     } else if mtd.file_type().is_dir() {
         println!("I am a directory");
     } else if mtd.file_type().is_symlink() {
         println!("I am a symbolic link");
     }
+    return std::process::ExitCode::from(0);
 }
 
-fn process_file(filename: &str) {
-    // Open
+// This function scrolls through "line major" or line by line.
+// This will not be default behavior but it is good as a first pass
+// It will return a vector of "matches"
+fn crep_file_line_major(filename: &str, pattern: &Regex) -> std::io::Result<u32> {
+    let mut total_matches: u32 = 0;
+
+    let fp = fs::File::open(filename)?;
+    let reader = BufReader::new(fp);
+
+    println!("Matches found in {filename}:");
+    for (index, line) in reader.lines().enumerate() {
+        let line = line?;
+
+        for ind_match in pattern.find_iter(&line) {
+            print_match(
+                index,
+                ind_match.start(),
+                ind_match.end() - ind_match.start(),
+                &line,
+            );
+        }
+    }
+    Ok(total_matches)
 }
+
+// Searches all files within a directory. Will automatically ignore certain types of files
+// fn crep_folder(foldername: &str, pattern: &str) -> std::io::Result<u32> {
+//     Ok(5)
+// }
+
+fn print_match(lineno: usize, colno: usize, length_of_match: usize, text: &str) {
+    println!("{lineno}:{text}");
+    println!(
+        "{}{}",
+        " ".repeat(lineno.to_string().len() + 1 + colno),
+        "^".repeat(length_of_match)
+    );
+}
+// fn chunk_file(fp: &fs::File) {}
